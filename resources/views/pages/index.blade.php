476e372972a6eae4d05eb3d7fa98f6dc327b651e
SopheakKhun <37277993+SopheakKhun@users.noreply.github.com>
2018-04-11 19:56:54 +1000

    Initial commit

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="jambotron text-center">
+    <h1> Welcome To Pioneer Web App</h1>
+    <p> This is the work on Professional Experience</p>
+
+    <p> <a class="btn btn-primary btn-lg" href="/login" role="button">Login</a> <a class="btn btn-success btn-lg" href="/register" role="button">Register</a></p>
+
+</div>
+
+@endsection
